596680aba2d371b2d2fe6af5f2fd5a587e98c8e5
SalAlba <saljan93@live.com>
2017-07-11 18:29:16 +0200

    Update Friend.h

--- Friends/1-Friend/Friend.h
+++ Friends/1-Friend/Friend.h
@@ -13,10 +13,14 @@ class Friend
         friend int pritnLength(Friend arg);
         friend void changePrivate(Friend arg);
 
+
     protected:
 
     private:
         int length;
+        friend void privateFunc(Friend fr);
+        void privFuncForClass();
+        friend class A;
 };
 
 #endif // FRIEND_H
