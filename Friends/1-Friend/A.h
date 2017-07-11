6a75766ea2cf4e18a6a3ebe68e6abcf8a2ef7751
SalAlba <saljan93@live.com>
2017-07-11 18:30:05 +0200

    Create A.h

---
+++ Friends/1-Friend/A.h
@@ -0,0 +1,18 @@
+#ifndef A_H
+#define A_H
+#include "Friend.h"
+
+class A
+{
+    public:
+        A();
+        virtual ~A();
+
+        void show(Friend f);
+
+    protected:
+
+    private:
+};
+
+#endif // A_H
