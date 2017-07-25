8c58a436664ec4acd2a7bad7d7fd259574a63168
SalAlba <saljan93@live.com>
2017-07-25 15:58:50 +0200

    Add files via upload

---
+++ Makefile/1/Body.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Body.h"
+
+using namespace std;
+
+int Body::funcpublic(){
+    cout << "public func \n";
+    return 0;
+}
---
+++ Makefile/1/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Body.h"
+
+using namespace std;
+
+int main(int argv, const char **args){
+
+    Body b;
+
+    cout << "hi there \n";
+    
+    b.funcpublic();
+
+
+    return 0;
+}
